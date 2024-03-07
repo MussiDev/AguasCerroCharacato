ea7fa65b14b356b38c912e9f5b832dc1b880840a
joakom24 <joakoomussi@gmail.com>
2024-03-07 15:11:50 -0300

    fix image

--- app/src/components/Banner.tsx
+++ app/src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import { Card } from "flowbite-react";
+import Image from "next/image";
 import React from "react";
-
+import { FaRegCircle } from "react-icons/fa";
 const BannerItem = ({
 	image,
 	title,
@@ -11,15 +12,16 @@ const BannerItem = ({
 	subtitle: string;
 }) => {
 	return (
-		<div className='flex flex-col justify-center items-center md:flex-row md:gap-48 h-screen'>
+		<div className='flex flex-col justify-center items-center md:flex-row  h-screen gap-4 md:gap-32 lg:gap-48'>
 			<Card href='#' className='max-w-md hover:bg-transparent bg-transparent '>
 				<h5 className='text-4xl font-bold tracking-tight text-white'>
 					{title}
 				</h5>
 				<p className='font-normal text-white'>{subtitle}</p>
 			</Card>
-
-			<img src={image} alt='' className='h-80' />
+			<div className='relative h-80 w-44'>
+				<Image layout='fill' alt='' src={image} />
+			</div>
 		</div>
 	);
 };
